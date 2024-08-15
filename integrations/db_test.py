4dd0da909ffa0f5406882c509d8a522c8f65e3f9
dpasse <50343386+dpasse@users.noreply.github.com>
2024-08-15 05:41:19 -0500

    more

--- integrations/db_test.py
+++ integrations/db_test.py
@@ -9,7 +9,8 @@ sys.path.insert(0, os.path.join(pathlib.Path(__file__).parent.resolve(), '../src
 
 from databurglar.models import setup_data_collection, setup_surveys, UserEvent
 from databurglar.helpers.connection import connect_to_pg, DatabaseConnection
-from databurglar.helpers.executors import insert
+from databurglar.helpers.executors import insert, query
+from databurglar.queries import tag
 
  
 if __name__ == '__main__':
@@ -33,3 +34,10 @@ if __name__ == '__main__':
     ]
 
     insert(engine, *data)
+
+    print(
+        query(
+            engine,
+            tag.ByIds.generate({uuid.uuid4()})
+        ).all()
+    )
--- src/databurglar/helpers/executors.py
+++ src/databurglar/helpers/executors.py
@@ -1,8 +1,13 @@
-from sqlalchemy import Engine
+from sqlalchemy import ChunkedIteratorResult, Engine, Select
 from sqlalchemy.orm import Session
 
 
-def insert(engine: Engine, *args):
+def insert(engine: Engine, *args) -> None:
     with Session(engine) as session:
         session.add_all(args)
         session.commit()
+
+
+def query(engine: Engine, statement: Select) -> ChunkedIteratorResult:
+    with Session(engine) as session:
+        return session.execute(statement)
